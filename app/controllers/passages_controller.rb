33725f30ab73780482a09907c616ed77ae63cafa
Anita Perala <anitaperala@gmail.com>
2015-09-07 08:07:27 -0400

    Add passage create method and index view

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -1,16 +1,21 @@
 class PassagesController < ApplicationController
 
+  before_filter :authorize
+  
   def index
+    @passages = Passage.all
   end
 
   def show
   end
 
   def new
+    @passage = Passage.new
   end
 
   def create
-
+    @passage = Passage.create(passage_params)
+    redirect_to passages_path
   end
 
   def edit
@@ -25,7 +30,7 @@ class PassagesController < ApplicationController
 
   private
   def passage_params
-    params.require(:passage).permit(:title, :author, :body, :link, :created_at, :updated_at)
+    params.require(:passage).permit(:title, :author, :body, :link, :user_id, :created_at, :updated_at)
   end
 
 end
