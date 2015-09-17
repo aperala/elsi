8096b7fca349a495196d57878ecc2df3be1d619c
Anita Perala <anitaperala@gmail.com>
2015-09-17 13:31:28 -0400

    Add password confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+
   has_many :passages, dependent: :destroy
   has_many :responses, dependent: :destroy
   
